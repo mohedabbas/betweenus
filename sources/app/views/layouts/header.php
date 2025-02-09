7598ae5666c93c9b52d0eaa8b5e50a18b93e1230
WilsonSan5 <wilson.san57@gmail.com>
2025-02-09 23:50:54 +0100

    change header logo to be a link to Home

--- sources/app/views/layouts/header.php
+++ sources/app/views/layouts/header.php
@@ -7,17 +7,17 @@ $userImg = $user['image_path'] ?? '/uploads/profiles/default.jpg';
 
 
 <header>
-    <nav>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <?php if (isset($_SESSION['user'])): ?>
-                <li><a href="/gallery">Gallery</a></li>
-                <li><a href="/logout">Logout</a></li>
-                <li><img src="<?= $userImg ?>" class="user__thumbnail"/> <?php echo $username ?></li>
-            <?php else: ?>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/register">Register</a></li>
-            <?php endif; ?>
-        </ul>
-    </nav>
+    <a href="/" class="brand">
+        <img src="../../../assets/images/brand-logo.png" alt="">
+        BetweenUs
+    </a>
+
+    <?php if (isset($_SESSION['user'])): ?>
+        <div class="user-profile">
+            <img src="<?= $userImg ?>" class="user__thumbnail" />
+            <p>
+                <?php echo $username ?>
+            </p>
+        </div>
+    <?php endif; ?>
 </header>

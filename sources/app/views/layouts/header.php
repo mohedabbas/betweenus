c2ad043ae0868e1b0873c0dc3dd2fec6a7795a44
Mohed Abbas <mohed.a@lmsfactory.com>
2025-02-09 23:44:55 +0100

    Some minor changes in header.php

--- sources/app/views/layouts/header.php
+++ sources/app/views/layouts/header.php
@@ -1,3 +1,11 @@
+<?php
+use App\Middlewares\AuthMiddleware;
+$user = AuthMiddleware::getSessionUser();
+$username = htmlspecialchars($user['username'] ?? 'Guest');
+$userImg = $user['image_path'] ?? '/uploads/profiles/default.jpg';
+?>
+
+
 <header>
     <nav>
         <ul>
@@ -5,6 +13,7 @@
             <?php if (isset($_SESSION['user'])): ?>
                 <li><a href="/gallery">Gallery</a></li>
                 <li><a href="/logout">Logout</a></li>
+                <li><img src="<?= $userImg ?>" class="user__thumbnail"/> <?php echo $username ?></li>
             <?php else: ?>
                 <li><a href="/login">Login</a></li>
                 <li><a href="/register">Register</a></li>
